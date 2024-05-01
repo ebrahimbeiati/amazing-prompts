c1f4def1c28d4ab22256a5f9365cc94a3c9501a7
Ebrahim Beiati-Asl <ebrahimbeiaty@gmail.com>
2024-05-01 11:21:24 +0100

    making components

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
+import Feed from "@components/Feed"
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+      <section className='w-full flex-center flex-col'>
+          <h1 className='head_text text-center'>
+              Discover & use
+              <br className='max-md:hidden' />
+              <span className='orange_gradient text-center'> Open AI Prompts</span>
+          </h1>
+          <p className="desc text-center">
+              Amazing prompts is an open-source AI prompting tool for modern creative. You can browse the collection below or generate your own.A collection of prompts for GPT-3 and Open AI.
+          </p>
+            <Feed />
+    </section>
   )
 }
 
